6cc01438c64ce84648988c5e91fe24247ea4ab0b
HomeJa <revanamike620@gmail.com>
2024-03-21 12:13:51 +0300

    test loader

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -1,3 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // After page is loaded, wait for 1 second and hide the loader
+  setTimeout(function () {
+      document.querySelector('.loader').classList.add('hidden');
+  }, 1900);
+});
+
 function handleMouseEvents(containerId, itemClass, datasetName) {
   const container = document.getElementById(containerId);
 
@@ -39,17 +46,18 @@ function startBackgroundLoop() {
     // Call the changeBackground function with the current index
     changeBackground(index);
 
-    // Check if it's the last index, if so, stop the loop
+    // Check if it's the last index, if so, end the loop
     if (index === maxIndex) {
       return;
     }
 
-    // Schedule the next iteration with a delay of 0.1 seconds (100 milliseconds)
+    // Schedule the next iteration with a delay of 0.25 seconds (250 milliseconds)
     setTimeout(function () {
       loopWithDelay(index + 1);
     }, 250);
   }
 }
+
 
 function changeContent(index) {
   // Update the picked-one attribute of the content element
@@ -207,3 +215,76 @@ function cursor() {
 }
 
 cursor(); // Call cursor function to initialize
+
+
+// loader
+class TextScramble {
+  constructor(el) {
+    this.el = el
+    this.chars = '!<>-_\\/[]{}—=+*^?#________'
+    this.update = this.update.bind(this)
+  }
+  setText(newText) {
+    const oldText = this.el.innerText
+    const length = Math.max(oldText.length, newText.length)
+    const promise = new Promise((resolve) => this.resolve = resolve)
+    this.queue = []
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || ''
+      const to = newText[i] || ''
+      const start = Math.floor(Math.random() * 40)
+      const end = start + Math.floor(Math.random() * 40)
+      this.queue.push({ from, to, start, end })
+    }
+    cancelAnimationFrame(this.frameRequest)
+    this.frame = 0
+    this.update()
+    return promise
+  }
+  update() {
+    let output = ''
+    let complete = 0
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i]
+      if (this.frame >= end) {
+        complete++
+        output += to
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar()
+          this.queue[i].char = char
+        }
+        output += `<span class="dud">${char}</span>`
+      } else {
+        output += from
+      }
+    }
+    this.el.innerHTML = output
+    if (complete === this.queue.length) {
+      this.resolve()
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update)
+      this.frame++
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)]
+  }
+}
+
+const phrases = [
+  'Bienvenue,'
+];
+
+const el = document.querySelector('.text-change')
+const fx = new TextScramble(el)
+
+let counter = 0
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 1200)
+  })
+  counter = (counter + 1) % phrases.length
+}
+
+next()
